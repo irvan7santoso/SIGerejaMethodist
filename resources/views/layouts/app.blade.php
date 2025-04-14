1c70834f36cd1b4e8d0a155ffd922aaa7f13c156
irvan7santoso <irvan7santoso@gmail.com>
2025-04-15 00:31:28 +0700

    login

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Sistem Informasi Gereja Methodist</title>
+        <meta content="" name="description">
+        <meta content="" name="keywords">
+      
+        <!-- Favicons -->
+        <link href="{{ asset('assets/img/favicon.png')}}" rel="icon">
+        <link href="{{ asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+      
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Vendor CSS Files -->
+        <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+        <link href="{{ asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
+
+        <!-- Template Main CSS File -->
+        <link href="{{ asset('assets/css/style.css')}}" rel="stylesheet">
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @isset($header)
+                <header class="bg-white shadow">
+                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                        {{ $header }}
+                    </div>
+                </header>
+            @endisset
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
+        </div>
+    </body>
+</html>
