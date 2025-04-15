91b00e629fc6badf31cf37659273212ac1daac52
irvan7santoso <irvan7santoso@gmail.com>
2025-04-15 09:32:43 +0700

    role

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -39,6 +39,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'jemaat', // Set default role here
         ]);
 
         event(new Registered($user));
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,14 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+
+        {{-- Tambahkan tombol register di bawah, di tengah --}}
+        @if (Route::has('register'))
+        <div class="flex items-center justify-center mt-4">
+            <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('Belum punya akun?') }}
+            </a>
+        </div>
+        @endif
     </form>
 </x-guest-layout>
